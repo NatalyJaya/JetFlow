429c98803e322927474e9d09186516deb8c0667c
GitHub Action <action@github.com>
2026-04-24 22:19:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/natalyjaya/jetflo/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/natalyjaya/jetflo/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.natalyjaya.jetflo.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
